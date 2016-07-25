27d014f030b7633082ecb11bbe4e82eda16eb329
nefa <ssfane@yahoo.com>
2016-07-26 01:30:24 +0300

    use old refs implementation

--- app/components/list-viewController.js
+++ app/components/list-viewController.js
@@ -3,30 +3,44 @@ import { Link } from 'react-router';
 
 
 export default class ListVCtrl extends Component {
+  constructor(props) {
+    super(props);
+    this.articleId = 0; //move to store later
+  }
+
+ /* componentDidMount() {
+    this._articleName.focus();
+    console.log(this._articleName)
+  }*/
+
+  addNewArticle() {
+    console.log(this.refs.articleName.value)
+    this.props.addArticle({
+      name: this.refs.articleName.value, 
+      id: ++this.articleId
+    });
+  }
 
   render() {
-    console.log(this.props, this)
     const { list, showMock, addArticle } = this.props;
-    return (<div>
-      <h4>this is where the larticle ist will be</h4>
-      <button onClick={showMock}>show mock list</button>
 
-      {list.map(item => {
-        return item 
-          ? <p key={item.id}>{item.name}</p>
-          : null;
-      })}
+    return <div>
+      <h4>this is where the larticle ist will be</h4>
+    
+      <button onClick={showMock}>show mock list</button>
+      {list.map(item => item 
+        ? <p key={item.id}>{item.name}</p>
+        : null
+      )}
 
       <p>
-        <button onClick={addArticle.bind(this, {
-          name: this._articleName, id: 1
-        })}>
+        <button onClick={e => this.addNewArticle()}>
                 add item
         </button>
         ||
-        <input type="text" ref={name => this._articleName = name}/>
+        <input type="text" ref="articleName"/>
       </p>
-    </div>)
+  </div>
   }
 
 }
