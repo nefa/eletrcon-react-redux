368c7668866ca4834b70b0047292444d2dc12a7c
nefa <ssfane@yahoo.com>
2016-07-26 22:09:57 +0300

    add Select component

--- app/components/list-viewController.js
+++ app/components/list-viewController.js
@@ -1,46 +1,69 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 
 export default class ListVCtrl extends Component {
+
   constructor(props) {
     super(props);
     this.articleId = 0; //move to store later
+    this.copyright = "";
   }
 
- /* componentDidMount() {
-    this._articleName.focus();
-    console.log(this._articleName)
-  }*/
-
   addNewArticle() {
-    console.log(this.refs.articleName.value)
+    console.log(this.refs.articleTitle.value)
     this.props.addArticle({
-      name: this.refs.articleName.value, 
-      id: ++this.articleId
+      title: this.refs.articleTitle.value,
+      author: this.refs.author.value,
+      copyright: this.copyright, 
+      id: ++this.articleId,
+      date: new Date()
     });
+  }
+
+  copyrightChange(c) {
+    console.log(c)
+    this.copyright = c.value;
   }
 
   render() {
     const { list, showMock, addArticle } = this.props;
+    const copyrightOptions = [
+      {value: 'All Rights Reserved', label: 'All Rights Reserved'},
+      {value: 'Some Rights Reserved', label: 'Some Rights Reserved'},
+      {value: 'No Rights Reserved', label: 'No Rights Reserved'}
+    ]
 
     return <div>
       <h4>this is where the larticle ist will be</h4>
     
       <button onClick={showMock}>show mock list</button>
       {list.map(item => item 
-        ? <p key={item.id}>{item.name}</p>
+        ? <p key={item.id}>{item.title}-{item.copyright}-{item.author}</p>
         : null
       )}
 
-      <p>
+      <div className="form">
+        title: <input type="text" ref="articleTitle"/>
+        <br />
+        author: <input type="text" ref="author"/>
+        <br />
+        content: <textarea></textarea>
+        <br />
+        licence: 
+        <Select name="slect-field"
+          value={copyrightOptions[0].value}
+          options={copyrightOptions}
+          onChange={this.copyrightChange.bind(this)}
+          />
+        <br /> 
         <button onClick={e => this.addNewArticle()}>
                 add item
         </button>
-        ||
-        <input type="text" ref="articleName"/>
-      </p>
-  </div>
+      </div>
+    </div>
   }
 
 }
--- app/reducers/list-reducer.js
+++ app/reducers/list-reducer.js
@@ -3,7 +3,7 @@ import { SHOW_MOCK , ADD_ART } from '../actions/list-actions';
 export default function list(state= [], action) {
   console.log("...list reducer invoked")
   if (action.type == SHOW_MOCK) 
-    return [{name: 'test', id: 0}]
+    return [{title: 'test', id: 0}]
 
   if (action.type == ADD_ART)
     return state.concat([action.payload]);
