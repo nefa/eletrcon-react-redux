cc6a86b08ba02557a1804ea5373a7b1cbfd049c3
nefa <ssfane@yahoo.com>
2016-08-08 00:55:21 +0300

    add a generic store service with namespace

--- app/reducers/localstorage-service.js
+++ app/reducers/localstorage-service.js
@@ -28,3 +28,57 @@ class StoreService {
 }
 
 export default new StoreService(/*window*/localStorage);
+
+
+const STORAGE = window.localStorage;
+
+class StorageService {
+  constructor(namespace) {
+    //set namespace 
+    this._ns = namespace;
+    
+    //load data form localstorage
+    var data = JSON.parse(STORAGE.getItem(this._ns));
+    
+    // set public _data on service state  
+    this._data = !!data ? data : []; 
+    /*synch with storage*/
+    if (!data) {
+      /*write an empty array*/
+      this.synchronize();
+    }  
+
+  }
+
+  synchronize() {// ovewrite the same namespace every time...
+    STORAGE.setItem(this._ns, JSON.stringify(this._data));
+  }
+
+  getAll() {
+    return this._data;
+  }
+
+  setItem(item) {
+    this._data.push(item);
+    this.synchronize();
+  }
+
+  deleteItem(id) {
+    this._data = this._data.filter(item => item.id != id);
+    this.synchronize();
+  }
+
+  editItem(id, itemObj) {
+    this._data = this._data
+      .filter(item => item.id != id);
+      .push(itemObj);
+    this.synchronize();  
+  }
+}
+
+
+
+
+
+
+
